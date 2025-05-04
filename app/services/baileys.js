a8687b5c1b7f8f734a459e071dcc1dfbcb1d1724
Jhainey Perez <jhainey.perez@dropi.co>
2025-05-04 16:16:06 -0500

    fix: improve logging for connection status in initBaileys function

--- app/services/baileys.js
+++ app/services/baileys.js
@@ -36,7 +36,7 @@ export default async function initBaileys() {
   sock.ev.on(
     "connection.update",
     async ({ connection, lastDisconnect, qr }) => {
-      console.log("connection", connection);
+      console.log("connection status", connection);
 
       if (qr) {
         const latestQRImg = await QRCode.toDataURL(qr);
