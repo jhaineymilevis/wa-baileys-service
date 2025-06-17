1cd9678d04a1ac31a5635235b80de83a492a2ca2
Jhainey Perez <jhainey.perez@dropi.co>
2025-06-16 19:16:20 -0500

    Fix caption

--- app/services/baileys.js
+++ app/services/baileys.js
@@ -114,7 +114,7 @@ export default async function initBaileys() {
 
         let quotedMessageText = getQuotedMessageText(quotedMessage);
 
-        text = ` ${text}  \n\nCita: ${quotedMessageText}`;
+        text = ` ${text}. Cita: ${quotedMessageText}`;
       }
 
       if (messageType == MESSAGE_TYPES.AUDIO) {
@@ -124,7 +124,7 @@ export default async function initBaileys() {
       if (messageType == MESSAGE_TYPES.IMAGE) {
         base64DataUri = await getImageStream(msg, sock);
 
-        text = `\n\nCaption: ${msg.message?.imageMessage?.caption || ""}`;
+        text = `Caption: ${msg.message?.imageMessage?.caption || ""}`;
       }
 
       console.log("📥 Tipo de mensaje:", messageType);
